fb674d13deb128639e6a9e8277563d192238da91
FedericoF03 <federicofernandeztec@gmail.com>
2022-09-12 01:51:01 -0300

    Se agrego la weather api

---
+++ Background.js
@@ -0,0 +1,26 @@
+
+const Background = ()=> {
+    let lat = null;
+    let long = null;
+    if(navigator.geolocation) {
+
+        let options = { enableHighAccuracy: true }
+        const err = (error)=>{
+            PEXELS("rain");
+        }
+        const success = async (pos)=>{
+            let pet = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`)
+            let res = await pet.json();
+            PEXELS(res.address.state);
+            Weather(res.address.state);
+        }
+        navigator.geolocation.getCurrentPosition( success, err, options)   
+    }
+    
+    
+    
+    
+    
+}
+
+Background();
--- WeatherApi.js
+++ WeatherApi.js
@@ -1,25 +1,17 @@
+const DATACURRENT = getId("temp")
+const DATACURRENTFl = getId("feelsLike")
+const DATACURRENTH = getId("humidity")
+const DATACURRENTW = getId("windSpeed")
+const DATACURRENTP = getId("probRain")
 
-const Background = ()=> {
-    let lat = null;
-    let long = null;
-    if(navigator.geolocation) {
-
-        let options = { enableHighAccuracy: true }
-        const err = (error)=>{
-            PEXELS("rain");
-        }
-        const success = async (pos)=>{
-            let pet = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`)
-            let res = await pet.json();
-            PEXELS(res.address.state);
-        }
-        navigator.geolocation.getCurrentPosition( success, err, options)   
-    }
-    
-    
-    
-    
-    
+const Weather = async(zone)=> {
+    let pet = await fetch(`http://api.weatherstack.com/current?access_key=6cfe1c9029e4eff281620177e2fdf54d&query=${zone}`);
+    let res = await pet.json()
+    DATACURRENT.textContent = 'Temperature:' + res.current.temperature + "°";
+    DATACURRENTFl.textContent = 'FeelsLike:' + res.current.feelslike + "°";
+    DATACURRENTH.textContent = 'humidity:' + res.current.humidity + "°";
+    DATACURRENTW.textContent = 'wind speed:' + res.current.wind_speed + "°";
+    DATACURRENTP.textContent = 'precip:' + res.current.precip + "°";
+    console.log(res);
 }
 
-Background();
