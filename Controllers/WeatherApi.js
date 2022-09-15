6a975a8ca139f30909f5f8eecdf5b19c903f6829
FedericoF03 <federicofernandeztec@gmail.com>
2022-09-14 21:50:46 -0300

    Modificaciones

--- Controllers/WeatherApi.js
+++ Controllers/WeatherApi.js
@@ -1,30 +1,33 @@
-const DATACURRENT = getId("temp")
-const DATACURRENTFl = getId("feelsLike")
-const DATACURRENTH = getId("humidity")
-const DATACURRENTW = getId("windSpeed")
-const DATACURRENTP = getId("probRain")
-const UBI = getId("ubi")
-const TEMPLATEDAILY = getId("template").content
+const DATACURRENT = getId("temp");
+const DATACURRENTFl = getId("feelsLike");
+const DATACURRENTH = getId("humidity");
+const DATACURRENTW = getId("windSpeed");
+const DATACURRENTP = getId("probRain");
+const UBI = getId("ubi");
+let search = getId("searchInputText");
+const space = getId("space");
+const TEMPLATEDAILY = getId("template").content;
 let frag = document.createDocumentFragment();
-let search = getId("searchInputText")
-const space = getId("space")
 
-const Weather = async(query, lat="", lon="")=> {
-    
-    const DATENOW = new Date()
+const Weather = async (query, lat="", lon="") => {
+
+    let stateZone = "";
+    let countryZone = "";  
+
+    const DATENOW = new Date();
+    let day = DATENOW.getDay() + 1;
     const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = DATENOW.getDay() + 1
-    let today = true
+    let today = true;
 
     if (query) {
         let petCoord = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value}&lang=es&appid=3fa0f591a653264d619986f2c14b8507&units=metric`);
         let resCoord = await petCoord.json();
         if (resCoord.cod === '404') {
-            return console.log("no se encontro")
+            return console.log("no se encontro");
         }
         PEXELS(search.value);
-        lat = resCoord.coord.lat    
-        lon = resCoord.coord.lon
+        lat = resCoord.coord.lat;   
+        lon = resCoord.coord.lon;
     } 
 
     let requestWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=3fa0f591a653264d619986f2c14b8507`;
@@ -32,12 +35,13 @@ const Weather = async(query, lat="", lon="")=> {
     let resWeather = await petWeather.json(); 
     let petZone = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`);
     let resZone = await petZone.json();
-    let stateZone = "";
+
     if(resZone.address.state !== undefined) stateZone = resZone.address.state;
-    let countryZone = "";
     if(resZone.address.country !== undefined) countryZone = resZone.address.country;
+
     if(!query) PEXELS(stateZone + " " + countryZone);
     UBI.textContent = stateZone + ", " + countryZone;
+
     DATACURRENT.textContent = 'Temperature: ' + resWeather.current.temp + "°C";
     DATACURRENTFl.textContent = 'FeelsLike: ' + resWeather.current.feels_like + "°C";
     DATACURRENTH.textContent = 'Humidity: ' + resWeather.current.humidity + "%";
@@ -45,26 +49,26 @@ const Weather = async(query, lat="", lon="")=> {
 
     if (space.children.length === 9) {
         while (1 !== space.children.length ) {
-            space.removeChild(space.children[1])
+            space.removeChild(space.children[1]);
         }
     }
             
     resWeather.daily.forEach(element => {
         if(day > 6) day = 0 
         if(today) {
-            TEMPLATEDAILY.getElementById("daytest").textContent = "Today"
+            TEMPLATEDAILY.getElementById("daytest").textContent = "Today";
             today = false;
         } else {
             TEMPLATEDAILY.getElementById("daytest").textContent = DAYS[day++];
-            TEMPLATEDAILY.getElementById("daytest").classList.add("other-day")
+            TEMPLATEDAILY.getElementById("daytest").classList.add("other-day");
         }
         
         if(DATENOW.getHours() > 18 || DATENOW.getHours() < 7 ) element.weather[0].icon = element.weather[0].icon.replace("d", "n")
         TEMPLATEDAILY.getElementById("clime").src = "http://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png";
-        TEMPLATEDAILY.getElementById("min").textContent = "min " + element.temp.min + "°"
-        TEMPLATEDAILY.getElementById("max").textContent = "max " + element.temp.max + "°"
+        TEMPLATEDAILY.getElementById("min").textContent = "min " + element.temp.min + "°C";
+        TEMPLATEDAILY.getElementById("max").textContent = "max " + element.temp.max + "°C";
         clone = document.importNode(TEMPLATEDAILY, true);
-        frag.appendChild(clone)
+        frag.appendChild(clone);
     });  
         space.appendChild(frag);             
 }
