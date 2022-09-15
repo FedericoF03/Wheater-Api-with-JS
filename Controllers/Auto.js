e7e7d8cc10470a8cfcc0ec9f6ee061482eca8aa2
FedericoF03 <federicofernandeztec@gmail.com>
2022-09-15 00:18:20 -0300

    Version final

--- Controllers/Auto.js
+++ Controllers/Auto.js
@@ -1,12 +1,24 @@
+const conteinerVideo = getId("conteinerVideo");
 const AUTO = ()=> {
     if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(pos => {
-            Weather(false, pos.coords.latitude, pos.coords.longitude);
-        })
-        
-    }
-    else console.log("No tiene compatibilidad");
-    
+        navigator.geolocation.getCurrentPosition(pos => Weather(false, pos.coords.latitude, pos.coords.longitude), 
+        () => {
+        let incompatibilidad = d.createElement("div");
+        let incompatibilidadMes = d.createElement("p");
+        incompatibilidad.classList.add("incompatibility"); 
+        incompatibilidadMes.classList.add("incompatibility-message");
+        incompatibilidadMes.textContent = "Su maquina o telefono celular no es compatible con la geocalizacion o no la tiene tiene activada";
+        incompatibilidad.appendChild(incompatibilidadMes);
+        conteinerVideo.appendChild(incompatibilidad);
+        }, { enableHighAccuracy: true } ); 
+    } else {
+        let incompatibilidad = d.createElement("div");
+        let incompatibilidadMes = d.createElement("p");
+        incompatibilidad.classList.add("incompatibility"); 
+        incompatibilidadMes.classList.add("incompatibility-message");
+        incompatibilidadMes.textContent = "Su maquina o telefono celular no es compatible con la geocalizacion o no la tiene tiene activada";
+        incompatibilidad.appendChild(incompatibilidadMes);
+        conteinerVideo.appendChild(incompatibilidad);
+    }    
 }
-
 AUTO();
