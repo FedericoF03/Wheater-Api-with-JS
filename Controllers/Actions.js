18ed95aa3e70475289a6fd7a0746ce01060e89ee
FedericoF03 <federicofernandeztec@gmail.com>
2022-10-05 15:59:06 -0300

    Se hicieron los cambios para solucionar un js

--- Controllers/Actions.js
+++ Controllers/Actions.js
@@ -2,7 +2,7 @@ let d = document
 const getId = id => d.getElementById(id);
 let button = getId("searchInputButton");
 const search = () => d.addEventListener('click', e => {
-    if (e.target == button) Weather(true)
+    if (e.target == button) weather(true)
 });
 
 export default search
